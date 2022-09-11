52c5edc5c55de60e38c91bb1229a92d4e619d050
Wzyan <3101885298@qq.com>
2022-09-11 23:23:14 +0800

    feat: 当变量作为参数且包含空格时，一定要在*传递时*包裹双引号，不然会被拆分为多个参数 如split \'\'

--- libs/functions/string.sh
+++ libs/functions/string.sh
@@ -1,4 +1,8 @@
 <<'COMMENT'
+1. 注意，当变量作为参数且包含空格时，一定要在*传递时*包裹双引号，不然会被拆分为多个参数 如split "$str"
+COMMENT
+#!/bin/bash
+<<'COMMENT'
 判断字符串1是否包含字符串2
 @case includesResult=(`includes a_b _`)
 @param fString  字符串1
@@ -27,11 +31,16 @@ function split () {
     local BAK_IFS=$IFS
     # 需要处理的字符串
     local str=$1
-    #设置分隔符
-    local IFS=$2
+    #设置分隔符 如果没有传入就按默认的空格拆分
+    echo ====$2
+    if [ ! $2 ]; then
+        local IFS=$2
+    fi
     #自动根据分隔符进行分割
     local FILE_LIST=($str)
     #恢复原来的分隔符
     IFS=$BAK_IFS
     echo ${FILE_LIST[@]}
 }
+# str="a b"
+# split "$str"
--- libs/index.sh
+++ libs/index.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 解决相对路径问题 执行脚本前先进入脚本所在的目录
-cd $(dirname $0)
+# cd $(dirname $0)
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
 source "./functions/module.sh"
 # 批量导入
 functionsDir=`pwd`/functions
@@ -10,3 +12,4 @@ glob $functionsDir sh
 result=(`split a_b _`)
 echo ${result[@]}
 COMMENT
+cd -
