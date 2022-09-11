53e8c63790fb9ef98338a4128c68d97e3d8ec94f
Wzyan <3101885298@qq.com>
2022-09-11 23:22:47 +0800

    feat: 当变量作为参数且包含空格时，一定要在*传递时*包裹双引号，不然会被拆分为多个参数 如split \'\'

--- libs/os-init/init-centos.sh
+++ libs/os-init/init-centos.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # 解决相对路径问题 执行脚本前先进入脚本所在的目录
 cd $(dirname $0)
+source "../index.sh"
 #===== sh远程连接
 ## 本地如果之前登陆过，需要执行下这个操作 不然登陆会报错
 ## rm -rf ~/.ssh/known_hosts 
@@ -8,13 +9,15 @@ cd $(dirname $0)
 #==== 先更新下源
 # yum update -y
 # git
-if [[  ]]; then
-source ../install/git/git.sh
-fi
+params=(`getParams $*`)
+echo params====${params[@]}
+# if [[  ]]; then
+# source ../install/git/git.sh
+# fi
 
 ## cat ~/.ssh/id_rsa.pub  这个公钥放在你自己的github上
-echo $#
-echo $0
+# echo $#
+# echo $0
 
 #==== node npm（使用nvm） 
-source ../install/nvm.sh
+# source ../install/nvm.sh
